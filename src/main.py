422c4a1d56033d546bbdc7e5c6f11dabd0c10f42
etduni <ETDUB13@ulaval.ca>
2025-03-08 22:23:09 -0500

    FIX : partially fix musée de Name Name

--- src/main.py
+++ src/main.py
@@ -5,7 +5,17 @@ texte = open('IApourTP1.txt',encoding='utf8').read()
 mots = re.split(r'[^\w|\.|-]+',texte)    
 
 
-sequence_ack = [["un"], ["au"], ["aux"], ["à"], ["en"], ["à", "l", "université"], ["de", "l", "université"], ["à", "l", "université", "d"], ["de", "l", "université", "d"], ["En"]]
+sequence_ack = [["un"],
+                ["au"],
+                ["aux"],
+                ["à"],
+                ["en"],
+                ["à", "l", "université"],
+                ["de", "l", "université"],
+                ["à", "l", "université", "d"],
+                ["de", "l", "université", "d"],
+                ["En"],
+                ["du", "musée", "de"]]
 
 for (idx, mot) in enumerate (mots) :
     if (len(mot) > 1 and mot[0].isupper() and mots[idx - 1][-1] != "." and mot[1].islower()) :
@@ -13,5 +23,7 @@ for (idx, mot) in enumerate (mots) :
             if (mots[idx-len(mot_ack):idx] == mot_ack) :
                 if (mots[idx+1] == "College") :
                     print(mots[idx-len(mot_ack):idx+2])
+                elif (mots[idx+1] == "de" and mots[idx+2][0].isupper()) :
+                    print(mots[idx-len(mot_ack):idx+3])
                 else :
                     print(mots[idx-len(mot_ack):idx+1])
