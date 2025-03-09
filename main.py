6257ccab0bf9874902e5827af1ebc85f082de157
etduni <ETDUB13@ulaval.ca>
2025-03-08 21:06:38 -0500

    ADD : split texte but keep period

--- main.py
+++
@@ -1,5 +0,0 @@
-import re
-
-texte = open('IApourTP1.txt',encoding='utf8').read()
-
-mots = re.split('\W+',texte)    
--- src/main.py
+++ src/main.py
@@ -2,4 +2,14 @@ import re
 
 texte = open('IApourTP1.txt',encoding='utf8').read()
 
-mots = re.split('\W+',texte)    
+mot_ack = ["un", "au", "aux", "à", "en"]
+mot_nack = ["l", "de", "le", "les"]
+
+mot_interposé = ["université"]
+
+mots = re.split(r'[^\w\.]+',texte)    
+
+print(mots)
+
+#for (idx, mot) in enumerate (mots) :
+
