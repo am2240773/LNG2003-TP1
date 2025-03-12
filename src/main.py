f9a35bf3523b77b880e0ab4e727a1994e93538af
Alexis Miron <2240773@cegeplimoilou.ca>
2025-03-12 13:31:48 -0400

    Script 2

--- src/main.py
+++ src/main.py
@@ -1,13 +1,30 @@
 import re
 
 texte = open('IApourTP1.txt',encoding='utf8').read()
-mots = re.split(r'[^\w|\.|-]+',texte)    
+mots = re.split(r'[^\w|\.|-]+',texte)
+
+# SCRIPT 1 &&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+#prep_avant = ["en", "au", "aux", "à"]
+#
+#for (idx, mot) in enumerate (mots) :
+#    if (len(mot) > 0 and mot[0].isupper()) :
+#        if (mots[idx-1].lower() in prep_avant) :
+#            print(mots[idx-1:idx+1])
+
+# SCRIPT 2 &&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
 prep_avant = ["en", "au", "aux", "à"]
 
 for (idx, mot) in enumerate (mots) :
     if (len(mot) > 0 and mot[0].isupper()) :
         if (mots[idx-1].lower() in prep_avant) :
-            print(mots[idx-1:idx+1])
+            if (not mot.isupper()) :
+                print(mots[idx-1:idx+1])
+            else :
+                for letter in mot :
+                    if not (letter in "IVXLCDM") :
+                        print(mots[idx-1:idx+1])
+                        break
 
 # Inclure "de" + Nom + Nom
