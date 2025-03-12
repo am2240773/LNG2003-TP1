d36eb3a5fbc40f7682c61b7b314aa56184b46354
etduni <ETDUB13@ulaval.ca>
2025-03-12 13:12:37 -0400

    FIX : Bonne version

--- src/main.py
+++ src/main.py
@@ -1,29 +1,13 @@
 import re
 
-
 texte = open('IApourTP1.txt',encoding='utf8').read()
 mots = re.split(r'[^\w|\.|-]+',texte)    
 
-
-sequence_ack = [["un"],
-                ["au"],
-                ["aux"],
-                ["à"],
-                ["en"],
-                ["à", "l", "université"],
-                ["de", "l", "université"],
-                ["à", "l", "université", "d"],
-                ["de", "l", "université", "d"],
-                ["En"],
-                ["du", "musée", "de"]]
+prep_avant = ["en", "au", "aux", "à"]
 
 for (idx, mot) in enumerate (mots) :
-    if (len(mot) > 1 and mot[0].isupper() and mots[idx - 1][-1] != "." and mot[1].islower()) :
-        for (seq_idx, mot_ack) in enumerate (sequence_ack) :
-            if (mots[idx-len(mot_ack):idx] == mot_ack) :
-                if (mots[idx+1] == "College") :
-                    print(mots[idx-len(mot_ack):idx+2])
-                elif (mots[idx+1] == "de" and mots[idx+2][0].isupper()) :
-                    print(mots[idx-len(mot_ack):idx+3])
-                else :
-                    print(mots[idx-len(mot_ack):idx+1])
+    if (len(mot) > 0 and mot[0].isupper()) :
+        if (mots[idx-1].lower() in prep_avant) :
+            print(mots[idx-1:idx+1])
+
+# Inclure "de" + Nom + Nom
