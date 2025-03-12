4da77d662b03cca4e291c3daa73cd9f45acfa7f0
etduni <ETDUB13@ulaval.ca>
2025-03-12 14:14:18 -0400

    Fix regex

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import re
 
 texte = open('IApourTP1.txt',encoding='utf8').read()
-mots = re.split(r'[^\w|\.|-]+',texte)
+mots = re.split(r'[^\w|-]+',texte)
 
 # SCRIPT 1 &&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
