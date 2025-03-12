535d65d1f3e7eb2e3168937507afe9e84b404d23
Alexis Miron <2240773@cegeplimoilou.ca>
2025-03-12 14:06:32 -0400

    script 2 men calisse

--- src/main.py
+++ src/main.py
@@ -20,11 +20,11 @@ for (idx, mot) in enumerate (mots) :
     if (len(mot) > 0 and mot[0].isupper()) :
         if (mots[idx-1].lower() in prep_avant) :
             if (not mot.isupper()) :
-                print(mots[idx-1:idx+1])
+                print(mots[idx-1:idx+3])
             else :
                 for letter in mot :
                     if not (letter in "IVXLCDM") :
-                        print(mots[idx-1:idx+1])
+                        print(mots[idx-1:idx+3])
                         break
 
 # Inclure "de" + Nom + Nom
