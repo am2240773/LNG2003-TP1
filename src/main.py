11b98b3450a7a3760c2be19f8ee039a8e01eb59c
etduni <ETDUB13@ulaval.ca>
2025-04-23 14:32:41 -0400

    Ajout patron 1

--- src/main.py
+++ src/main.py
@@ -1,26 +1,43 @@
+class occurence_classe:
+  def __init__(self, defini_len, definition_depart, definition_len):
+    self.defini_len = defini_len
+    self.definition_depart = definition_depart
+    self.definition_len = definition_len
+
+dict_occurence = []
+
 import re
 T = open('TALpourTP2.txt',encoding='utf8')
 texte = T.read()
 
-#for r in re.finditer(r'.{,75} est un .{,75}',texte,re.S):
-#	print(r.group(0))
-#	print('*' * 25)
-
-# Prépartation
-    # 1. Pré-traitrement pour éviter couper les abréviations
+# 1. Pré-traitrement pour éviter couper les abréviations
 texte = re.sub(r'\(abr. ([A-Z]+)\)', '(abr_ \\1)', texte)
 
-    # 2. Divisier le texte en phrase
-phrase = re.findall(r'([A-Z].+?[^.].(\.[ \n]|\n\n))', texte, re.S)
+# 2. Divisier le texte en phrase
+phrase = re.findall(r'([A-Z].+?[^.].\.[ \n]|\n\n)', texte, re.S)
 
-    # 3. Post-traitement pour restaurer les abréviations
+# 3. Post-traitement pour restaurer les abréviations
 for ph in phrase:
-    p = ph[0]
-    p = re.sub('abr_', 'abr.', p)
-# Analyse
-    # 4. Sortir les éléments définis et définitoires à l'aide de regex
-    print(p.rstrip())
-    print('\n')
+    ph = re.sub('abr_', 'abr.', ph[0])
 
-# Présentation
-    # 5. Impression des résultats sur la console avec contexte
+# 4. Sortir les éléments définis et définitoires à l'aide de regex
+
+# Sortir les mots suivants le patron 1
+for ph in phrase:
+    #res_regex = re.findall(r'([A-Z]+?[\sA-Z]*?), ([\w ]*)', ph, re.S)
+    res_regex = re.findall(r'(\b[A-Z]+[\sA-Z]+), ([\w ]*)', ph, re.S)
+    print(res_regex)
+    
+
+# 5. Impression des résultats sur la console avec contexte
+#for r in re.finditer(r'.{,75} est un .{,75}',texte,re.S):
+
+
+
+#### Liste des patrons ####
+
+#https://regex101.com/r/vltxNk/1
+#du nom de doit précéder patron 1
+
+# PATRON 1 : ... NOM_DEFINI, definition sur plusieurs mots jusqu'à frontière
+
