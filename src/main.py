bd2657b7ac7c11c86aca359bf5489c1b4d053b63
etduni <ETDUB13@ulaval.ca>
2025-04-24 20:11:10 -0400

    PATRON C

--- src/main.py
+++ src/main.py
@@ -26,3 +26,23 @@ for ph in phrase:
         if len(hit) > 0 :
             print(str(hit[0]) + " est un " + str(hit[1]))
 
+# PATRON C : DEFINI : definition ,/.
+for ph in phrase:
+    res_regex = re.findall(r'([A-Z][\w|\s]*) : (.*)[.|,]', ph)
+    
+    for hit in res_regex:
+        if len(hit) > 0 :
+            print(str(hit[0]) + " : " + str(hit[1]))
+
+# PATRON à faire
+# qui vise à                                    3
+# parfois appelé | qui sera appelé              4 7
+# Maj word word (ALLCAPS)                       5 49 51
+# comporte                                      8
+# du nom d' / du nom de                         9 11
+# Name Name, word word,                         10
+# << >> (name)                                  12
+# Maj , ./,                                     16 17 18
+# ne sont qu'                                   30
+# il s'agit de                                  31 32 52
+# Maj est ...                                   48
